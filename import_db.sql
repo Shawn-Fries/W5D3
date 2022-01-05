0b61440a26ec3298e33c584cb09e7c9f2ae249fe
egumin <ethangumin@gmail.com>
2022-01-05 11:22:13 -0500

    create tables

--- import_db.sql
+++ import_db.sql
@@ -17,9 +17,42 @@ CREATE TABLE questions (
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY
-    question INTEGER NOT NULL
-    user INTEGER NOT NULL
+    question_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL
 
-    
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 )
+
+CREATE TABLE replies (  
+    id INTEGER PRIMARY KEY
+    subject_question_id INTEGER NOT NULL
+    parent_reply_id INTEGER
+    author_id INTEGER NOT NULL
+    body VARCHAR(255) NOT NULL
+
+    FOREIGN KEY (subject_question_id) REFERENCES questions(id)
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+    FOREIGN KEY (author_id) REFERENCES users(id)
+)
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY
+    user_id INTEGER NOT NULL
+    question_id INTEGER NOT NULL
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Shawn', 'Fries'),
+    ('Ethan', 'Gumin')
+
+INSERT INTO
+    questions (title, body, associated_author)
+VALUES
+    ("How do I create a SQL database?", "See Title", )
 
