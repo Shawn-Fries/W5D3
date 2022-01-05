3d79e3ae620763c9afd5ac8b77191fc0b2aba75d
egumin <ethangumin@gmail.com>
2022-01-05 13:51:07 -0500

    next commit

--- question.rb
+++ question.rb
@@ -1,5 +1,12 @@
 require_relative 'questions_database.rb'
 
 class Question
+    def initialize(new_question)
+        @first_name = new_question['fname']
+        @last_name = new_question['lname']
+    end
 
+    def self.find_by_id
+        Question.new
+    end
 end
