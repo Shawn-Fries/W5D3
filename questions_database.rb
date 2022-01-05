e8dbdc449fa1eaa37f43bccac18794ac322a69de
egumin <ethangumin@gmail.com>
2022-01-05 14:30:27 -0500

    fix questions_database.rb

--- questions_database.rb
+++ questions_database.rb
@@ -1,12 +1,12 @@
 require 'sqlite3'
 require 'singleton'
 
-SQLite3::Database.new( "questions.db" ) do |db|
-  db.execute( "select * from users" ) do |row|
-    p row
-  end
-end
-
 class QuestionsDatabase < SQLite3::Database
     include Singleton
+
+    def initialize
+      super('questions.db')
+      self.type_translation = true
+      self.results_as_hash = true
+    end
 end
--- user.rb
+++ user.rb
@@ -2,11 +2,26 @@ require_relative 'questions_database.rb'
 
 class User < QuestionsDatabase
 
-    #def self.find_by_id
-    #    User.instance.
-    #end
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+        data.map { |datum| User.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        User.new(QuestionsDatabase.instance.execute("SELECT * FROM users WHERE id = #{id}"))
+    end
+
+    def self.find_by_name(fname, lname)
+        User.new(QuestionsDatabase.instance.execute(<<-SQL, fname, lname) 
+                                                    SELECT *
+                                                     FROM users 
+                                                     WHERE fname = #{fname}
+                                                     AND lname = #{lname}
+                                                     SQL)
+    end
 
     def initialize(new_question)
+        @id = new_question['id']
         @first_name = new_question['fname']
         @last_name = new_question['lname']
     end
